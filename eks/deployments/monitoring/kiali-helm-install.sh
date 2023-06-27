c9abe8a491ca6c913a313f9230ead8b3c585cac8
Vladislav Stefanov <vladislav.stefanov@news.co.uk>
2023-06-27 20:17:54 +0300

    add prometheus and grafana deployments, include kiali helm installation scripts

---
+++ eks/deployments/monitoring/kiali-helm-install.sh
@@ -0,0 +1,11 @@
+helm repo add kiali https://kiali.org/helm-charts
+
+helm repo update
+
+ helm install \
+    --set cr.create=true \
+    --set cr.namespace=istio-system \
+    --namespace kiali-operator \
+    --create-namespace \
+    kiali-operator \
+    kiali/kiali-operator
